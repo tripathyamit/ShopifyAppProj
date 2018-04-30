43ce394a9849ee594e65a3dd9cb67c4647763661
tripathyamit <37783210+tripathyamit@users.noreply.github.com>
2018-04-30 05:56:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ class Config(object):
     SECRET_KEY = "CantStopAddictedToTheShinDigChopTopHeSaysImGonnaWinBigg"
     HOST = "dc7708e6.ngrok.io"
     SHOPIFY_CONFIG = {
-        'API_KEY': 'f0f78f97a5b0b2390357591295ff81cd',
-        'API_SECRET': '54bc48af11f66f7847854821281fd556',
+        'API_KEY': '',
+        'API_SECRET': '',
         'APP_HOME': 'http://' + HOST,
         'CALLBACK_URL': 'http://' + HOST + '/install',
         'REDIRECT_URI': 'http://' + HOST + '/connect',
